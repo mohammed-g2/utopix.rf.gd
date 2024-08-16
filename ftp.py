cbacc388057a9a4b52807542ea4148c3ca7aeb21
Mohammed <mohammed.mm404@gmail.com>
2024-08-16 22:08:21 +0300

    updated ftp.by

--- ftp.py
+++ ftp.py
@@ -8,13 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-ftp = ftplib.FTP(
-    os.environ.get('FTP_SERVER'),
-    os.environ.get('FTP_USERNAME'),
-    os.environ.get('FTP_PASSWORD'))
 
-
-def delete_ftp_dirs(ftp):
+def delete_ftp_dirs(ftp: ftplib.FTP):
     def remove_ftp_dir(path):
         for (name, properties) in ftp.mlsd(path=path):
             if name in ['.', '..']:
@@ -39,15 +34,16 @@ def delete_ftp_dirs(ftp):
             remove_ftp_dir(item[0])
 
 
-def upload(ftp, path):
+def upload(ftp: ftplib.FTP, path: str):
     """path: path to upload from"""
     for name in os.listdir(path):
-        localpath = os.path.join(path, name)
-        if os.path.isfile(localpath):
-            print("STOR", name, localpath)
-            ftp.storbinary('STOR ' + name, open(localpath,'rb'))
-        elif os.path.isdir(localpath):
-            print("MKD", name)
+        local_path = os.path.join(path, name)
+        if os.path.isfile(local_path):
+            print('Created File:', name, local_path)
+            with open(local_path,'rb') as f:
+                ftp.storbinary('STOR ' + name, f)
+        elif os.path.isdir(local_path):
+            print('Created Directory:', name)
 
             try:
                 ftp.mkd(name)
@@ -57,14 +53,14 @@ def upload(ftp, path):
                 if not e.args[0].startswith('550'): 
                     raise
 
-            print("CWD", name)
+            print("Moving to:", name)
             ftp.cwd(name)
-            upload(ftp, localpath)           
-            print("CWD", "..")
+            upload(ftp, local_path)           
+            print("Moving to:", "..")
             ftp.cwd("..")
 
 
-def remove_folder_with_permission(repo):
+def remove_folder_with_permission(repo: str):
     for root, dirs, files in os.walk(repo):  
         for dir in dirs:
             os.chmod(os.path.join(root, dir), stat.S_IRWXU)
@@ -78,52 +74,84 @@ def cli():
     pass
 
 
-@click.command()
 def delete_remote():
-    """clear the contents of htdocs in remote ftp server."""
-    ftp.cwd('htdocs')
+    """clear the contents of UPLOAD_TO in remote ftp server."""
+    ftp = ftplib.FTP(
+        os.environ.get('FTP_SERVER'),
+        os.environ.get('FTP_USERNAME'),
+        os.environ.get('FTP_PASSWORD'))
+
+    ftp.cwd(os.environ['UPLOAD_TO'])
+
     delete_ftp_dirs(ftp)
+    
     ftp.quit()
     print('Done.')
 
 
-@click.command('clone-repo')
 def clone_repo():
     """clone github repo and clear .git file."""
     print('Cloning repo...')
     try:
         git.Repo.clone_from(os.environ.get('GITHUB_REPO'), 'website')
+
         print('removing .git folder')
         remove_folder_with_permission(os.path.join('website', '.git'))
+        
         print('Done.')
     except Exception as e:
         print(str(e))
 
 
-@click.command('upload')
 def ftp_upload():
-    """upload website to remote htdocs ftp directory."""
-    ftp.cwd('htdocs')
+    """upload website to remote UPLOAD_TO ftp directory."""
+    ftp = ftplib.FTP(
+        os.environ.get('FTP_SERVER'),
+        os.environ.get('FTP_USERNAME'),
+        os.environ.get('FTP_PASSWORD'))
+
+    ftp.cwd(os.environ.get('UPLOAD_TO'))
+
     print('Uploading...')
     upload(ftp, 'website')
+    
     print('Removing source')
     remove_folder_with_permission('website')
+    
     ftp.quit()
     print('Done.')
 
 
+@click.command('delete-remote')
+def delete_remote_cmd():
+    """clear the contents of UPLOAD_TO in remote ftp server."""
+    delete_remote()
+
+
+@click.command('clone-repo')
+def clone_repo_cmd():
+    """clone github repo and clear .git file."""
+    clone_repo()
+
+
+@click.command('upload')
+def upload_cmd():
+    """upload website to remote UPLOAD_TO ftp directory."""
+    ftp_upload()
+
+
 @click.command('streamline')
-def streamline():
+def streamline_cmd():
     """run all commands."""
     delete_remote()
     clone_repo()
     ftp_upload()
 
 
-cli.add_command(delete_remote)
-cli.add_command(clone_repo)
-cli.add_command(ftp_upload)
-cli.add_command(streamline)
+cli.add_command(delete_remote_cmd)
+cli.add_command(clone_repo_cmd)
+cli.add_command(upload_cmd)
+cli.add_command(streamline_cmd)
 
 if __name__ == '__main__':
     cli()
