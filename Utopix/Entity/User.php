2603eb4c1baf53fcbd325a937df72b47ddfe2654
Mohammed <mohammed.mm404@gmail.com>
2024-08-18 06:10:38 +0300

    added user permissions

--- Utopix/Entity/User.php
+++ Utopix/Entity/User.php
@@ -11,8 +11,15 @@ class User
     public string $email;
     public string $password;
     public string $about_me;
+    public int $permissions;
     private DatabaseTable $posts;
     private ?array $userPosts;
+
+    const EDIT_POST = 1;
+    const DELETE_POST = 2;
+    const EDIT_CATEGORY = 4;
+    const DELETE_CATEGORY = 8;
+    const EDIT_USER_ACCESS = 16;
 
     public function __construct(DatabaseTable $posts)
     {
@@ -24,6 +31,9 @@ class User
         return sprintf('<UserEntity %s>', $this->username);
     }
 
+    /**
+     * get posts written by user
+     */
     public function getPosts() 
     {
         if (empty($this->userPosts)) {
@@ -36,5 +46,41 @@ class User
     {
         $post['user_id'] = $this->id;
         return $this->posts->save($post);
+    }
+
+    /**
+     * check if user have given permission
+     */
+    public function hasPermission(int $permission): bool
+    {
+        return $this->permissions & $permission;
+    }
+
+    /**
+     * add new permission to user
+     */
+    public function addPermission(int $permission)
+    {
+        if (!$this->hasPermission($permission)) {
+            $this->permissions += $permission;
+        }
+    }
+
+    /**
+     * remove permission from user
+     */
+    public function removePermission($permission) 
+    {
+        if ($this->hasPermission($permission)) {
+            $this->permissions -= $permission;
+        }
+    }
+
+    /**
+     * remove all permissions from user
+     */
+    public function resetPermissions()
+    {
+        $this->permissions = 0;
     }
 }
--- includes/routes.php
+++ includes/routes.php
@@ -14,15 +14,18 @@ $website->addRoute('auth/logout', ['GET', 'POST'], ['Auth', 'logout']);
 $website->addRoute('posts/home', ['GET'], ['Posts', 'homePage']);
 $website->addRoute('posts/list', ['GET'], ['Posts', 'list']);
 $website->addRoute('posts/get', ['GET'], ['Posts', 'get']);
-$website->addRoute('posts/create', ['GET', 'POST'], ['Posts', 'create'], true);
-$website->addRoute('posts/update', ['GET', 'POST'], ['Posts', 'update'], true);
-$website->addRoute('posts/delete', ['POST'], ['Posts', 'homePage'], true);
+$website->addRoute('posts/create', ['GET', 'POST'], ['Posts', 'create'],
+    true, \Utopix\Entity\User::EDIT_POST);
+$website->addRoute('posts/update', ['GET', 'POST'], ['Posts', 'update'],
+    true, \Utopix\Entity\User::EDIT_POST);
+$website->addRoute('posts/delete', ['POST'], ['Posts', 'homePage'],
+    true, \Utopix\Entity\User::DELETE_POST);
 
 // Users controller routes
 $website->addRoute('users/list', ['GET'], ['Users', 'list'], true);
 $website->addRoute('users/get', ['GET'], ['Users', 'get']);
 $website->addRoute('users/create', ['GET', 'POST'], ['Users', 'create']);
-$website->addRoute('users/update', ['GET', 'POST'], ['Users', 'update'], true);
+$website->addRoute('users/update', ['GET', 'POST'], ['Users', 'update']);
 $website->addRoute('users/delete', ['Post'], ['Users', 'delete'], true);
 
 // Error routes
