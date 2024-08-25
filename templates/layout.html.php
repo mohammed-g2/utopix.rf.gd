760027021a446b1227a872e290b0dd16b11c8528
Mohammed <mohammed.mm404@gmail.com>
2024-08-25 17:03:49 +0300

    template fix

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -9,6 +9,7 @@
   <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
   <!-- Third-party CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css"> 
   <!-- App CSS -->
   <link rel="stylesheet" href="/assets/css/base.css">
 
