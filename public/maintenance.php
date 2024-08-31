ac869ad5729c924e2bb54f66336b6eb1a4fe845a
Mohammed <mohammed.mm404@gmail.com>
2024-09-01 02:42:24 +0300

    updated maintenance page

--- public/maintenance.php
+++ public/maintenance.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta name="description" content="Utopix">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
     <title>Utopix</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css"> -->
 </head>
 
-<body>
+<body class="h-100">
     <div class="container-fluid h-100 w-100 p-0 m-0">
         <ul class="nav bg-dark">
             <li class="nav-item">
@@ -19,17 +19,20 @@
             </li>
         </ul>
 
-        <div class="row h-100 w-100 m-0 p-0">
+        <div class="row w-100 h-100 m-0 p-0">
             <div class="col-md-6 col-12 h-100">
-                <div class="d-flex align-items-center h-100 ps-5">
-                    <div>
+                <div class="ps-5">
+                    <div class="mt-5 mb-5 ">
                         <h1>Under Maintenance</h1>
-                        <p>Our website is currently under maintenance, we will be here shortly.</p>
+                        <p>Our website is currently under maintenance, we will be back shortly.</p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-12 h-100 m-0 p-0">
-                <img id="page-image" src="./assets/images/lazy-cat.png" alt="a lazy cat">
+                <img 
+                    src="/assets/images/lazy-cat.png" 
+                    alt="a lazy cat" 
+                    style="object-fit:cover;width:100%;height:100%;">
             </div>
         </div>
     </div>
